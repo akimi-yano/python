ff37f43eb766dea7ffa83d469fed2436f1a59dd3
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-02-04 10:12:00 -0800

    modified a file

--- python_oop/user/user.py
+++ python_oop/user/user.py
@@ -6,32 +6,32 @@ class User:
 
     def make_deposit(self, amount):
         if amount>0:
-                self.account_balance += amount
-                print("desposit of $" + str(amount) +" completed")
+            self.account_balance += amount
+            print("desposit of $" + str(amount) +" completed")
         else:
             print("amount tyring to widthdraw is smaller than or equal to $0")
 
     def make_withdrawal(self, amount):
         if amount>0 and amount<self.account_balance:  
-                self.account_balance -= amount
-                print("withdrawal of $" + str(amount) +" completed")
+            self.account_balance -= amount
+            print("withdrawal of $" + str(amount) +" completed")
         elif amount <=0:
             print("amount tyring to widthdraw is smaller than or equal to $0")
         else:
             print(str(self.name) + "'s current balance is smaller than the amount trying to withdraw")
     
     def display_user_balance(self):
-            print(str(self.name) + "'s current balance is $" + str(self.account_balance))
+        print(str(self.name) + "'s current balance is $" + str(self.account_balance))
 
     def transfer_money(self, other_user, amount):
-            if amount<self.account_balance:
-                self.account_balance -= amount
-                other_user.account_balance += amount
-                print(str(self.name) + " successfully transferred $" + str(amount) + " to "+ str(other_user.name))
-                print(str(self.name) + "'s current balance is $" + str(self.account_balance))
-                print(str(other_user.name) + "'s current balance is $" + str(other_user.account_balance))
-            else:
-                print(str(self.name) + "'s current balance is smaller than the amount trying to transfer")
+        if amount<self.account_balance:
+            self.account_balance -= amount
+            other_user.account_balance += amount
+            print(str(self.name) + " successfully transferred $" + str(amount) + " to "+ str(other_user.name))
+            print(str(self.name) + "'s current balance is $" + str(self.account_balance))
+            print(str(other_user.name) + "'s current balance is $" + str(other_user.account_balance))
+        else:
+            print(str(self.name) + "'s current balance is smaller than the amount trying to transfer")
 
 # #USER1
 akimi = User("Akimi", "akimi@gmail.com")
@@ -47,9 +47,9 @@ akimi.display_user_balance()
 # # # #USER2
 yuta = User("Yuta", "yuta@gmail.com")
 print(yuta.name)
-yuta.make_deposit(1000), 
 yuta.make_deposit(1000)
-yuta.make_withdrawal(500), 
+yuta.make_deposit(1000)
+yuta.make_withdrawal(500)
 yuta.make_withdrawal(500)
 yuta.display_user_balance()
 
