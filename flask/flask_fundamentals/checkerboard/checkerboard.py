7a5b1a203c690f574d11973404bc483bb0c312c3
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-02-06 21:44:43 -0800

    modified files

--- flask/flask_fundamentals/checkerboard/checkerboard.py
+++ flask/flask_fundamentals/checkerboard/checkerboard.py
@@ -10,15 +10,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def eight_by_eight():
-    return render_template("index.html", num_height = 8, num_width = 8)
+    return render_template("index.html", num_height = 8, num_width = 8, custom_colors=False)
 
 @app.route("/4")
 def eight_by_four():
-    return render_template("index.html", num_height = 8, num_width = 4)
+    return render_template("index.html", num_height = 8, num_width = 4, custom_colors=False)
+
+@app.route("/<number>")
+def number_by_five(number):
+    return render_template("index.html", num_height = int(number), num_width = 5, custom_colors=False)
 
 @app.route("/<number1>/<number2>")
 def x_by_y(number1, number2):
-    return render_template("index.html", num_height = int(number1), num_width = int(number2))
+    return render_template("index.html", num_height = int(number1), num_width = int(number2), custom_colors=False)
+
+@app.route("/<number1>/<number2>/<color1>/<color2>")
+def x_by_y_colors(number1, number2, color1, color2):
+    return render_template("index.html", num_height = int(number1), num_width = int(number2), color1 = color1, color2 = color2, custom_colors=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
