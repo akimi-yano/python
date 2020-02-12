949394330da6bd493a900021f5e00fd7f64c44d7
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-02-11 21:36:10 -0800

    modified files

--- django/django-intro/ninja_gold/ninja/urls.py
+++ django/django-intro/ninja_gold/ninja/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_money', views.process_money)
+    # path('process_money', views.process_money),
+    path('rules',views.rules),
+    #Set more specific ones first and more free ones later
+    path('<str:location>', views.process_money),
 ]
--- django/django-intro/ninja_gold/ninja/views.py
+++ django/django-intro/ninja_gold/ninja/views.py
@@ -5,38 +5,39 @@ from time import gmtime, strftime
 def index(request):
     if 'gold' not in request.session:
         request.session['gold'] = 0
-   
     return render(request, "index.html")
 
+def process_money(request, location):
 
-def process_money(request):
-    # print("In process!!")
-    if request.method == "POST":
-        val_from_forms= request.POST["which_form"]
+    # if request.method == "POST":
+    #     val_from_forms= request.POST["which_form"]
 
-    if val_from_forms == 'farm':
+    # if val_from_forms == 'farm':
+    if location == 'farm':
         if 'addition' in request.session:
              del request.session['addition'] 
         request.session['addition'] = random.randint(10,20)
-        # print(f"printing for test{val_from_forms}")
         request.session['gold'] = request.session['gold'] + request.session['addition'] 
         result = f"Earned {request.session['addition']} golds from the farm! "
   
-    if val_from_forms == 'cave':
+    # if val_from_forms == 'cave':
+    if location == 'cave':
         if 'addition' in request.session:
             del request.session['addition'] 
         request.session['addition'] = random.randint(5,10)
         request.session['gold'] = request.session['gold'] + request.session['addition'] 
         result = f"Earned {request.session['addition']} golds from the cave! "
 
-    if val_from_forms == 'house': 
+    # if val_from_forms == 'house': 
+    if location == 'house':
         if 'addition' in request.session:
             del request.session['addition']      
         request.session['addition'] = random.randint(2,5)
         request.session['gold'] = request.session['gold'] + request.session['addition'] 
         result = f"Earned {request.session['addition']} golds from the house! "
        
-    if val_from_forms == 'casino':
+    # if val_from_forms == 'casino':
+    if location == 'casino':
         if 'addition' in request.session:
             del request.session['addition'] 
         request.session['addition'] = random.randint(-50, 50)
@@ -52,5 +53,17 @@ def process_money(request):
         request.session['activities'] = []
     time = strftime("%Y-%m-%d %H:%M %p", gmtime())
     request.session['activities'].append([result, time])
+
+    if 'attempts' not in request.session:
+        request.session['attempts'] = 1
+    elif 'attempts' in request.session:
+        request.session['attempts'] += 1
+    return redirect('/')
+
+def rules(request):
+    if request.method == "POST" and 'goal' not in request.session and 'moves' not in request.session:
+        request.session['goal']= int(request.POST['goal'])
+        request.session['moves']= int(request.POST['moves'])
+        request.session['attempts'] = 0
     return redirect('/')
     
